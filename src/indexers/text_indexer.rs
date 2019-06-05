eb3b25d85a065374e48db0f1e3ba97d9f5130666
Teodor Voinea <voinea.teodor@gmail.com>
2019-06-05 18:12:55 -0400

    Add some super basic tests to get things going

--- src/indexers/text_indexer.rs
+++ src/indexers/text_indexer.rs
@@ -19,3 +19,23 @@ impl Indexer for TextIndexer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_indexing_text_file() {
+        let test_file_path = Path::new("./test_files/file.txt");
+        let indexed_document = TextIndexer::index_file(test_file_path);
+
+        assert_eq!(indexed_document.name, "file.txt");
+        assert_eq!(indexed_document.body, "this is a file with some contents in it");
+    }
+
+    #[test]
+    fn test_supports_text_extension() {
+        assert_eq!(true, TextIndexer::supports_extension(OsStr::new("txt")));
+        assert_eq!(false, TextIndexer::supports_extension(OsStr::new("png")));
+    }
+}
