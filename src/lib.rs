1b6e598a74ff68a2bd8039eaa9a17ce5dbf2f0c6
Teodor Voinea <voinea.teodor@gmail.com>
2020-04-04 13:56:21 -0700

    Remove unnecessary extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,7 @@
-extern crate app_dirs;
-extern crate blake2b_simd;
-extern crate calamine;
-extern crate config;
-extern crate csv;
-extern crate docx;
-extern crate exif;
-extern crate image;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate log;
-extern crate msoffice_pptx;
-extern crate msoffice_shared;
-extern crate notify;
-extern crate pdf_extract;
-extern crate regex;
-extern crate reverse_geocoder;
-extern crate serde_derive;
-extern crate serde_json;
-extern crate simple_logger;
-extern crate tantivy;
-extern crate tract_tensorflow;
 
 pub mod indexers;
 pub mod query_executor;
