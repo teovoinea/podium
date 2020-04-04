f8ca111a811faf9e1878d6acdf22d1103c0027e1
Teodor Voinea <voinea.teodor@gmail.com>
2020-04-04 15:49:18 -0700

    Mini refactor to pull actix web functionality into a routes folder

--- src/bin.rs
+++ src/bin.rs
@@ -3,6 +3,8 @@ use std::thread;
 extern crate podium_lib;
 use podium_lib::query_executor::QueryResponse;
 use podium_lib::tantivy_process::start_tantivy;
+use podium_lib::routes::{search};
+use podium_lib::contracts::{AppState::*};
 
 #[macro_use]
 extern crate log;
@@ -12,42 +14,6 @@ use std::io;
 use actix_cors::Cors;
 use actix_web::{http, middleware, web, App, HttpRequest, HttpResponse, HttpServer};
 use crossbeam::channel::{unbounded, Receiver, Sender};
-
-struct AppState {
-    query_sender: Sender<String>,
-    result_receiver: Receiver<QueryResponse>,
-}
-
-async fn index(app_state: web::Data<AppState>, req: HttpRequest) -> HttpResponse {
-    println!("{:?}", req);
-
-    let query: String = req.match_info().query("query").parse().unwrap();
-    println!("{:?}", query);
-
-    println!("Getting query channel");
-    let query_channel = &app_state.query_sender;
-
-    println!("Sending from query channel");
-    if let Err(err) = query_channel.send(query) {
-        println!("{:?}", err);
-    }
-
-    println!("Getting response channel");
-    let resp = &app_state.result_receiver;
-
-    println!("Getting from response channel");
-    let result = match resp.recv() {
-        Err(err) => format!("Err: {:?}", err),
-        Ok(r) => {
-            println!("Ok!: {:?}", r);
-            serde_json::to_string(&r).unwrap()
-        }
-    };
-
-    info!("Found results: {:?}", &result);
-
-    HttpResponse::Ok().body(result)
-}
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -72,7 +38,7 @@ async fn main() -> io::Result<()> {
                     .finish(),
             )
             .app_data(app_state.clone())
-            .route("/search/{query}", web::get().to(index))
+            .configure(search::config)
     })
     .bind("127.0.0.1:8080")?
     .run()
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ extern crate log;
 pub mod indexers;
 pub mod query_executor;
 pub mod tantivy_process;
+pub mod routes;
+pub mod contracts;
 
 mod file_watcher;
 
