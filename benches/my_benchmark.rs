9e5306cbaadfa0f9144e56518e39767afb70b016
Teodor Voinea <voinea.teodor@gmail.com>
2021-03-15 12:22:33 -0700

    Comment benches for a bit

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,102 +1,104 @@
-#[macro_use]
-extern crate criterion;
-extern crate podium_lib;
+// #[macro_use]
+// extern crate criterion;
+// extern crate podium_lib;
 
-use podium_lib::contracts::file_to_process::FileToProcess;
-use podium_lib::indexers::*;
+// use podium_lib::contracts::file_to_process::FileToProcess;
+// use podium_lib::indexers::*;
 
-use criterion::black_box;
-use criterion::Criterion;
+// use criterion::black_box;
+// use criterion::Criterion;
 
-use std::path::Path;
+// use std::path::Path;
 
-fn bench_indexing_csv_file(c: &mut Criterion) {
-    c.bench_function("indexing_csv_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/data.csv"));
-            CsvIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_csv_file(c: &mut Criterion) {
+//     c.bench_function("indexing_csv_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/data.csv"));
+//             CsvIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-fn bench_indexing_exif_file(c: &mut Criterion) {
-    c.bench_function("indexing_exif_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/IMG_2551.jpeg"));
-            ExifIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_exif_file(c: &mut Criterion) {
+//     c.bench_function("indexing_exif_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/IMG_2551.jpeg"));
+//             ExifIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-#[cfg(not(target_os = "windows"))]
-fn bench_indexing_mobile_net_v2_file(c: &mut Criterion) {
-    c.bench_function("indexing_mobile_net_v2_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/IMG_2551.jpeg"));
-            MobileNetV2Indexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// #[cfg(not(target_os = "windows"))]
+// fn bench_indexing_mobile_net_v2_file(c: &mut Criterion) {
+//     c.bench_function("indexing_mobile_net_v2_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/IMG_2551.jpeg"));
+//             MobileNetV2Indexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-fn bench_indexing_pdf_file(c: &mut Criterion) {
-    c.bench_function("indexing_pdf_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/Cats.pdf"));
-            PdfIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_pdf_file(c: &mut Criterion) {
+//     c.bench_function("indexing_pdf_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/Cats.pdf"));
+//             PdfIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-fn bench_indexing_pptx_file(c: &mut Criterion) {
-    c.bench_function("indexing_pptx_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/Cats.pptx"));
-            PptxIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_pptx_file(c: &mut Criterion) {
+//     c.bench_function("indexing_pptx_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/Cats.pptx"));
+//             PptxIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-fn bench_indexing_spreadsheet_file(c: &mut Criterion) {
-    c.bench_function("indexing_spreadsheet_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/Cats.xlsx"));
-            SpreadsheetIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_spreadsheet_file(c: &mut Criterion) {
+//     c.bench_function("indexing_spreadsheet_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/Cats.xlsx"));
+//             SpreadsheetIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-fn bench_indexing_text_file(c: &mut Criterion) {
-    c.bench_function("indexing_text_file", |b| {
-        b.iter(|| {
-            let bench_file_path = black_box(Path::new("./test_files/file.txt"));
-            TextIndexer.index_file(&FileToProcess::from(bench_file_path))
-        });
-    });
-}
+// fn bench_indexing_text_file(c: &mut Criterion) {
+//     c.bench_function("indexing_text_file", |b| {
+//         b.iter(|| {
+//             let bench_file_path = black_box(Path::new("./test_files/file.txt"));
+//             TextIndexer.index_file(&FileToProcess::from(bench_file_path))
+//         });
+//     });
+// }
 
-#[cfg(not(target_os = "windows"))]
-criterion_group!(
-    benches,
-    bench_indexing_csv_file,
-    bench_indexing_exif_file,
-    bench_indexing_mobile_net_v2_file,
-    bench_indexing_pdf_file,
-    bench_indexing_exif_file,
-    bench_indexing_pptx_file,
-    bench_indexing_spreadsheet_file,
-    bench_indexing_text_file,
-);
+// #[cfg(not(target_os = "windows"))]
+// criterion_group!(
+//     benches,
+//     bench_indexing_csv_file,
+//     bench_indexing_exif_file,
+//     bench_indexing_mobile_net_v2_file,
+//     bench_indexing_pdf_file,
+//     bench_indexing_exif_file,
+//     bench_indexing_pptx_file,
+//     bench_indexing_spreadsheet_file,
+//     bench_indexing_text_file,
+// );
 
-#[cfg(target_os = "windows")]
-criterion_group!(
-    benches,
-    bench_indexing_csv_file,
-    bench_indexing_exif_file,
-    bench_indexing_pdf_file,
-    bench_indexing_exif_file,
-    bench_indexing_pptx_file,
-    bench_indexing_spreadsheet_file,
-    bench_indexing_text_file,
-);
+// #[cfg(target_os = "windows")]
+// criterion_group!(
+//     benches,
+//     bench_indexing_csv_file,
+//     bench_indexing_exif_file,
+//     bench_indexing_pdf_file,
+//     bench_indexing_exif_file,
+//     bench_indexing_pptx_file,
+//     bench_indexing_spreadsheet_file,
+//     bench_indexing_text_file,
+// );
 
-criterion_main!(benches);
+// criterion_main!(benches);
+
+fn main() {}
